e683902ce75a7c0cc8ddb0c75a622d55a98444dd
johnpatrick254 <jpattrick538@gmail.com>
2024-06-10 16:04:14 +0300

    configure gh

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/react-ts-test/",
     element: <App />,
   },
 ]);
